4bc91fb6e5cc35b367c8177a982cfe53d4bc9e18
hansbabu-scaleios <114058359+hansbabu-scaleios@users.noreply.github.com>
2024-10-07 14:40:56 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,16 @@ export NVM_DIR=$HOME/.nvm;
 . $NVM_DIR/nvm.sh;
 
 cd /home/ubuntu/deploy_nextjs_app
+
+# Stash any local changes before pulling the latest code
+git stash --include-untracked
+
+# Pull the latest code from the repository
 git pull origin master
+
+# Apply the stashed changes back (if any)
+git stash pop || echo "No local changes to reapply."
+
 nvm use v16
 yarn install &&
 yarn build &&
