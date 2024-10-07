99990b0f7dbee029df2e4c17ee760ea02d62b70d
hansbabu-scaleios <114058359+hansbabu-scaleios@users.noreply.github.com>
2024-10-07 14:34:02 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -32,7 +32,7 @@ export default function Home() {
           <h2 className={inter.className}>
             5minslearn <span>-&gt;</span>
           </h2>
-          <p className={inter.className}>Learn tech in 25mins</p>
+          <p className={inter.className}>Learn tech in 95mins</p>
         </a>
       </div>
     </main>
